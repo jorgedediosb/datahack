3fed58640f3e4dee61cbc7901aa5362fe7644569
jorgedediosb <jorgededios@icloud.com>
2024-05-20 17:51:56 +0200

    update sql files

--- SQL/2_Clausulas.sql
+++ SQL/2_Clausulas.sql
@@ -15,3 +15,41 @@ SELECT departamento, COUNT(*) as total_empleados FROM empleados GROUP BY departa
 
 --6. WHERE: Se usa para filtrar filas basadas en una condición específica:
 SELECT * FROM productos WHERE precio > 100;
+
+--7. JOIN: combina filas de dos o más tablas:
+    -- INNER JOIN: Combina filas de dos tablas donde existe una coincidencia en ambas.
+    SELECT clientes.cliente_id, clientes.nombre, pedidos.pedido_id, pedidos.fecha
+    FROM clientes
+    INNER JOIN pedidos ON clientes.cliente_id = pedidos.cliente_id;
+
+    -- LEFT JOIN. Devuelve todas las filas de la tabla izquierda y las filas coincidentes de la tabla derecha.
+    SELECT clientes.cliente_id, clientes.nombre, pedidos.pedido_id, pedidos.fecha
+    FROM clientes
+    LEFT JOIN pedidos ON clientes.cliente_id = pedidos.cliente_id;
+
+    -- RIGHT JOIN. Devuelve todas las filas de la tabla derecha y las filas coincidentes de la tabla izquierda.
+    SELECT clientes.cliente_id, clientes.nombre, pedidos.pedido_id, pedidos.fecha
+    FROM clientes
+    RIGHT JOIN pedidos ON clientes.cliente_id = pedidos.cliente_id;
+
+    -- FULL JOIN. Devuelve todas las filas cuando hay una coincidencia en una de las tablas.
+    SELECT clientes.cliente_id, clientes.nombre, pedidos.pedido_id, pedidos.fecha
+    FROM clientes
+    FULL JOIN pedidos ON clientes.cliente_id = pedidos.cliente_id;
+
+    -- CROSS JOIN. Devuelve el producto cartesiano de las dos tablas.
+    SELECT clientes.cliente_id, clientes.nombre, pedidos.pedido_id, pedidos.fecha
+    FROM clientes
+    CROSS JOIN pedidos;
+
+--8. UNION. 
+/*
+se utiliza para combinar los resultados de dos o más consultas SELECT en un solo conjunto de resultados.
+La cláusula UNION selecciona solo filas distintas de ambas consultas.
+Si deseas incluir todas las filas, incluidas las duplicadas, puedes utilizar UNION ALL
+*/
+
+SELECT cliente_id, nombre FROM clientes_2023
+UNION
+SELECT cliente_id, nombre FROM clientes_2024;
+
