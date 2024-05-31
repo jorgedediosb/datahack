b6496c1380c0516ce604f38d17add5ba3d53cbf5
jorgedediosb <jorgededios@icloud.com>
2024-05-31 11:25:01 +0200

    update kafka files

--- Kafka/ProyectoFinal/SentimentAnalyst/app/consumer.py
+++ Kafka/ProyectoFinal/SentimentAnalyst/app/consumer.py
@@ -1,3 +1,4 @@
+
 from confluent_kafka import Consumer, KafkaException
 from textblob import TextBlob
 from pymongo import MongoClient
@@ -56,3 +57,58 @@ def consume_messages():
 
 if __name__ == '__main__':
     consume_messages()
+'''
+from confluent_kafka import Consumer, KafkaException
+import os
+from textblob import TextBlob
+from pymongo import MongoClient
+
+kafka_broker = os.getenv('KAFKA_BROKER', 'broker:29092')
+kafka_topic = os.getenv('KAFKA_TOPIC', 'input-topic')
+kafka_group_id = os.getenv('KAFKA_GROUP_ID', 'sentiment_group')
+mongo_uri = os.getenv('MONGO_URI', 'mongodb://admin:admin@mongo:27017/')
+
+# Configurar el consumidor de Kafka
+consumer = Consumer({
+    'bootstrap.servers': kafka_broker,
+    'group.id': kafka_group_id,
+    'auto.offset.reset': 'earliest'
+})
+
+# Conectar a MongoDB
+client = MongoClient(mongo_uri)
+db = client.sentiment_analysis
+collection = db.sentiments
+
+def analyze_sentiment(text):
+    blob = TextBlob(text)
+    return blob.sentiment.polarity, blob.sentiment.subjectivity
+
+def consume_loop(consumer, topics):
+    try:
+        consumer.subscribe(topics)
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    raise KafkaException(msg.error())
+            text = msg.value().decode('utf-8')
+            polarity, subjectivity = analyze_sentiment(text)
+            sentiment_data = {
+                'text': text,
+                'polarity': polarity,
+                'subjectivity': subjectivity
+            }
+            print(f"Polarity: {polarity}, Subjectivity: {subjectivity}")
+            collection.insert_one(sentiment_data)
+    finally:
+        consumer.close()
+
+if __name__ == '__main__':
+    consume_loop(consumer, [kafka_topic])
+
+'''
--- Kafka/ProyectoFinal/SentimentAnalyst/app/producer.py
+++ Kafka/ProyectoFinal/SentimentAnalyst/app/producer.py
@@ -1,3 +1,4 @@
+
 from confluent_kafka import Producer
 import os
 
@@ -18,3 +19,71 @@ with open('data.txt', 'r') as file:
         p.poll(1)
 
 p.flush()
+'''
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from confluent_kafka import Producer
+import os
+
+kafka_broker = os.getenv('KAFKA_BROKER', 'broker:29092')
+kafka_topic = os.getenv('KAFKA_TOPIC', 'input-topic')
+data_file = 'data.txt'
+progress_file = 'progress.txt'
+
+# Configurar el productor de Kafka
+producer = Producer({'bootstrap.servers': kafka_broker})
+
+def acked(err, msg):
+    if err is not None:
+        print(f"Failed to deliver message: {err}")
+    else:
+        print(f"Message produced: {msg.value().decode('utf-8')}")
+
+def read_progress():
+    if not os.path.exists(progress_file):
+        return 0
+    with open(progress_file, 'r') as f:
+        try:
+            return int(f.readline().strip())
+        except ValueError:
+            return 0
+
+def write_progress(lines_processed):
+    with open(progress_file, 'w') as f:
+        f.write(str(lines_processed))
+
+class FileChangeHandler(FileSystemEventHandler):
+    def __init__(self):
+        super().__init__()
+        self.lines_processed = read_progress()
+        self.process_new_lines(initial=True)
+
+    def on_modified(self, event):
+        if event.src_path == os.path.abspath(data_file):
+            self.process_new_lines()
+
+    def process_new_lines(self, initial=False):
+        with open(data_file, 'r') as f:
+            lines = f.readlines()
+            new_lines = lines[self.lines_processed:]
+            for line in new_lines:
+                producer.produce(kafka_topic, line.strip(), callback=acked)
+                producer.flush()
+                self.lines_processed += 1
+            write_progress(self.lines_processed)
+
+if __name__ == '__main__':
+    event_handler = FileChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path='.', recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+'''
+
